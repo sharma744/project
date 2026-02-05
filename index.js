98859ba3502be048871d5b2b99dd167510f4cf0c
aayusharma <aayusharma9711@gmail.com>
2026-02-05 22:09:54 +0530

    fourth commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let bcrypt=require("bcrypt")
 let app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-let conn=mongoose.connect("mongodb+srv://kayaar450_db_user:fDKrEjSbnVV1uIxS@cluster0.rzb4emq.mongodb.net//pet",{
+let conn=mongoose.connect("mongodb+srv://kayaar450_db_user:fDKrEjSbnVV1uIxS@cluster0.rzb4emq.mongodb.net/pet",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
