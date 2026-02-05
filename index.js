9c265bf47fed15d9c4dc500bcc4e75038765e35a
aayusharma <aayusharma9711@gmail.com>
2026-02-05 22:15:06 +0530

    fifth commit

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let bcrypt=require("bcrypt")
 let app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-let conn=mongoose.connect("mongodb+srv://kayaar450_db_user:fDKrEjSbnVV1uIxS@cluster0.rzb4emq.mongodb.net/pet",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+let conn=mongoose.connect("mongodb+srv://kayaar450_db_user:fDKrEjSbnVV1uIxS@cluster0.rzb4emq.mongodb.net/pet");
 let db=mongoose.connection;
 db.once("open",()=>{
     console.log("database connected succesfully")
