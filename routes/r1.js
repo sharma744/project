fd720fa4e85385e9b3a810230049d472e8cfafac
aayusharma <aayusharma9711@gmail.com>
2026-02-05 22:21:21 +0530

    sixth commit

--- routes/r1.js
+++ routes/r1.js
@@ -1,6 +1,6 @@
-let control=require("/Users/aayushsharma/indiafluence_project/serverside/controller/c1.js")
+let control=require("../controller/c1")
 let multer=require("multer")
-let upload=multer({dest:"/Users/aayushsharma/indiafluence_project/serverside/uploads"})
+let upload=multer({dest:"../controller/uploads"})
 let middlwr=require("../middelware/otp_gen")
 route=(app)=>{
     app.post("/auth/login",control.login)
