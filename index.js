238bd639602468f762839e182854fece46d66741
aayusharma <aayusharma9711@gmail.com>
2026-02-07 00:51:01 +0530

    eleventh commit

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ db.on("error",()=>{
 require("./routes/r1")(app)
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
